3edf757079f7ff996209727b039c406dd1e1c644
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-09-05 15:06:12 +0500

    added two seprate sections or cover and JD

---
+++ src/components/CoverLetter.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const CoverLetter = () => {
+  const [cover, setCover] = useState('');
+
+  return (
+    <>
+      <h1>Cover Letter</h1>
+    </>
+  )
+}
+
+export default CoverLetter
---
+++ src/components/JobDescription.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const JobDescription = () => {
+  const [jobDescription, setJobDescription] = useState('');
+
+  return (
+    <>
+      <h1>Job Description</h1>
+    </>
+  )
+}
+
+export default JobDescription
